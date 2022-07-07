37440fb581bf7b11d3b65a2345b66d4780e7be7b
Visal <visal.saosuo@gmail.com>
2022-07-07 23:16:35 +0700

    fix recodeVector to be vectorize

--- SML_RC/ex4-octave/recodeVector.m
+++ SML_RC/ex4-octave/recodeVector.m
@@ -1,13 +1,15 @@
 function recodedVect = recodeVector(val, vectSize)
 %recodeVector - takes an input value, val, and convert into an a matrix
-% with a dimension of 1 x vectSize. The matrix will conpute based on
+% with a dimension of length(val) x vectSize. The matrix will conpute based on
 % ones and zeros, in which all elements are zeros except at column val that
 % equals to one.
 if(vectSize < val)
-  error("cannot complete the operation because vectSize < val.");
+  error("cannot complete the operation.");
 else
-  recodedVect = zeros(1, vectSize);
-  recodedVect(val) = 1;
+  recodedVect = zeros(length(val), vectSize);
+  for(i = 1:length(val))
+    recodedVect(i, val(i)) = 1;
+  end
 endif
 % =========================================================================
 
